cf20d9b6f3201d587f659202ec9921e27cb726dc
dzhurba <idmitrij2008@yandex.ru>
2022-02-15 19:00:35 +0300

    WorkerFactory start

--- app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -1,12 +1,29 @@
 package com.example.cryptoapp.presentation
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.di.ApplicationComponent
 import com.example.cryptoapp.data.di.DaggerApplicationComponent
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 
-class CoinApp : Application() {
+class CoinApp : Application(), Configuration.Provider {
 
     val component: ApplicationComponent by lazy {
         DaggerApplicationComponent.factory().create(this)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    coinInfoDao = AppDatabase.getInstance(this).coinPriceInfoDao(),
+                    apiService = ApiFactory.apiService,
+                    mapper = CoinMapper()
+                )
+            )
+            .build()
     }
 }
