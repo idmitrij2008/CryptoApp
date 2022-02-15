6f172c3d2fdf701d4f0fa0a4eeb96eb0168c813d
dzhurba <idmitrij2008@yandex.ru>
2022-02-15 18:43:41 +0300

    add DI

---
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.presentation
+
+import android.app.Application
+import com.example.cryptoapp.data.di.ApplicationComponent
+import com.example.cryptoapp.data.di.DaggerApplicationComponent
+
+class CoinApp : Application() {
+
+    val component: ApplicationComponent by lazy {
+        DaggerApplicationComponent.factory().create(this)
+    }
+}
